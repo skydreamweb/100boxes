119c117cc95c7b5f0c305519752546a68528bd0b
Dalibor <geneeje@yahoo.com>
2019-10-17 18:04:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ canvas.setWidth(601);
 
 //Sets height of this canvas instance
 canvas.setHeight(601);
+
+// Define 'pre-game' status
+let gameStage = 'pre-game';
+
+// Define time/click counters
+let counterTime = 0;
+let counterClick = 0;
+let int1 = 0;
