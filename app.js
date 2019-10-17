92a4cc11a82688ed1d51cd4703ca3f08b75b789a
Dalibor <geneeje@yahoo.com>
2019-10-17 17:38:33 +0200

    Import eslint settings
    task-boilerplate/.eslintrc-react

---
+++ app.js
@@ -0,0 +1,8 @@
+// Reference canvas element (with id="c")
+const canvas = new fabric.Canvas("c");
+
+// Sets width of this canvas instance
+canvas.setWidth(601);
+
+//Sets height of this canvas instance
+canvas.setHeight(601);
