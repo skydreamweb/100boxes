28d89dbc8daef5a1449a5a566ab17dcab7b74121
Dalibor <geneeje@yahoo.com>
2019-10-18 00:37:30 +0200

    regularClick function

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ let firstClick = function (e, clickedField) {
         // Y coordinate = clickedField.y coordinate + second value of each array of coordinates array
         const y = clickedField.y + affectedCombos[i][1];
 
-        // Push object in array with properties x/y and value of coordinates
+        // Push results object in array with properties x/y and value of coordinates
         affectedFields.push({
             x,
             y
@@ -129,4 +129,59 @@ let firstClick = function (e, clickedField) {
     }
     // Renders both the top canvas and the secondary container canvas.
     canvas.renderAll();
+}
+
+// Second += 1 click /Regular Click function 
+let regularClick = function (e, clickedField) {
+    // If statment - if clickedField has property yellow then fill green and change 
+    // the value of color propety to green 
+    if (clickedField.color === 'yellow') {
+        clickedField.set('fill', 'green');
+        clickedField.color = 'green';
+        // Array of all affected fields around clickedField
+        const affectedFields = [];
+        // For loop trough coordinates array
+        for (let i = 0; i < coordinates.length; i += 1) {
+            // X coordinate = clickedField.x coordinate + first value of each array of coordinates array
+            const x = clickedField.x + coordinates[i][0];
+            // Y coordinate = clickedField.y coordinate + second value of each array of coordinates array
+            const y = clickedField.y + coordinates[i][1];
+            // Push results in object in array with properties x/y and value of coordinates
+            affectedFields.push({
+                x,
+                y
+            });
+        }
+        // Loop trough all of 100 objects and fill all fields with #fbfcf2 unless they are 'green'
+        // Add change color propety to white
+        for (let i = 0; i < e.target.canvas._objects.length; i += 1) {
+            if (e.target.canvas._objects[i].color !== 'green') {
+                e.target.canvas._objects[i].set('fill', '#fbfcf2');
+                e.target.canvas._objects[i].color = 'white';
+
+                // Loop and fill all white fields following x,y coordinates 
+                for (let j = 0; j < coordinates.length; j += 1) {
+                    // If object.x == affectedFields.x then color yellow  
+                    if (e.target.canvas._objects[i].x === affectedFields[j].x &&
+                        e.target.canvas._objects[i].y === affectedFields[j].y) {
+                        e.target.canvas._objects[i].set('fill', 'yellow');
+                        e.target.canvas._objects[i].color = 'yellow';
+                    }
+                }
+            }
+        }
+        canvas.renderAll();
+
+        // Loop if there is yellow buttons left
+        for (let i = 0; i < e.target.canvas._objects.length; i += 1) {
+            if (e.target.canvas._objects[i].color == 'yellow') {
+                counterClick += 1
+
+                // If there is yellow buttons - repeat regularClick function
+                return
+            }
+        }
+        // if not start gameOver() function
+        gameOver(e.target);
+    }
 }
